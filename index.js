6f71b5355abf6b081471ad2899c42979e574e2cd
Abhishek Raj <118152707+Abhishekbotx@users.noreply.github.com>
2024-06-17 15:10:28 +0530

    vercel file upload

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express')
-const { PORT } = require('./src/config/dotenvConfig') 
+const { PORT } = require('./src/config/dotenvConfig')
 const cookieParser = require("cookie-parser");
-const db=require('./src/config/dbConfig')
-const cors = require('cors') 
-const app = express(); 
+const db = require('./src/config/dbConfig')
+const cors = require('cors')
+const app = express();
 
 
 
-app.use(  
-  cors({ 
-    origin: "https://trash-passing.vercel.app", 
-      credentials: true, 
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    }) 
-  );
+app.use(
+    cors({
+        origin: "https://trash-passing.vercel.app",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+);
 
 const apiRoutes = require('./src/routes/index');
 app.use(cookieParser());
@@ -21,9 +21,9 @@ app.use(cookieParser());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send(`<h1> HomePage Says Hi </h1>`);
-  });
+});
 const prepareAndStartServer = () => {
-    app.use('/api', apiRoutes); 
+    app.use('/api', apiRoutes);
     db.connect()
     app.listen(PORT, () => {
         console.log(`server running on port:${PORT}`)
